36c3a1ac09f8820fc1a3db27b02c7f63b39bf217
mridul-agrawal <48617300+mridul-agrawal@users.noreply.github.com>
2021-07-19 21:28:09 +0530

    Scripted Animation for Horizontal Input and Flipping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,33 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-   
+    public Animator playerAnimator;
 
+    private void Update()
+    {
+        HandleHorizontalInput();
+    }
+
+    public void HandleHorizontalInput()
+    {
+        float speed = Input.GetAxisRaw("Horizontal");
+
+        if (speed < 0)
+        {
+            Vector3 scale = transform.localScale;
+            scale.x = -1 * Mathf.Abs(scale.x);
+            transform.localScale = scale;
+
+            speed = Mathf.Abs(speed);
+        }
+        else if (speed > 0)
+        {
+            Vector3 scale = transform.localScale;
+            scale.x = Mathf.Abs(scale.x);
+            transform.localScale = scale;
+        }
+
+        playerAnimator.SetFloat("speed", speed);
+    }
 
 }
