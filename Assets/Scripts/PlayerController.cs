89077ae93283113646fcefc496cad295837fe22c
mridul-agrawal <48617300+mridul-agrawal@users.noreply.github.com>
2021-07-20 20:36:21 +0530

    Implemented Crouch Animation and accepted Vertical Input for Jump Animation.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,9 +6,16 @@ public class PlayerController : MonoBehaviour
 {
     public Animator playerAnimator;
 
+    private Vector2 standingColliderOffset = new Vector2(0f, 0.98f);
+    private Vector2 standingColliderSize = new Vector2(0.45f, 2.05f);
+    private Vector2 crouchingColliderOffset = new Vector2(-0.15f, 0.6f);
+    private Vector2 crouchingColliderSize = new Vector2(0.75f,1.3f);
+
     private void Update()
     {
         HandleHorizontalInput();
+        HandleVerticalInput();
+        HandleOtherInput();
     }
 
     public void HandleHorizontalInput()
@@ -31,6 +38,32 @@ public class PlayerController : MonoBehaviour
         }
 
         playerAnimator.SetFloat("speed", speed);
+    }
+
+    public void HandleVerticalInput()
+    {
+
+    }
+
+    public void HandleOtherInput()
+    {
+        if(Input.GetKey(KeyCode.LeftControl))
+        {
+            // Crouch
+            playerAnimator.SetBool("Crouch",true);
+            GetComponent<BoxCollider2D>().offset = crouchingColliderOffset;
+            GetComponent<BoxCollider2D>().size = crouchingColliderSize;
+        } else if(Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            //Stand
+            playerAnimator.SetBool("Crouch", false);
+            GetComponent<BoxCollider2D>().offset = standingColliderOffset;
+            GetComponent<BoxCollider2D>().size = standingColliderSize;
+        } else if(Input.GetKeyDown(KeyCode.Space))
+        {
+            // Jump
+            playerAnimator.SetTrigger("Jump");
+        }
     }
 
 }
