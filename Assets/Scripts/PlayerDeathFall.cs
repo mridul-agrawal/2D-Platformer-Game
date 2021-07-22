09512694aeb7299dda7ec7618380190fe9f16638
mridul-agrawal <48617300+mridul-agrawal@users.noreply.github.com>
2021-07-22 14:01:09 +0530

    Implemented Player Death UI on falling from the platform

---
+++ Assets/Scripts/PlayerDeathFall.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerDeathFall : MonoBehaviour
+{
+    public Camera mainCamera;
+    public CanvasRenderer deathUIPanel;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.GetComponent<PlayerController>() != null)
+        {
+            mainCamera.transform.parent = null;
+            deathUIPanel.gameObject.SetActive(true);
+            GetComponent<PlayerController>().gameObject.SetActive(false);
+        }
+    }
+}
